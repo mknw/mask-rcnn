b61c958c76766e5917a53ef1b2276aee794b8599
mknw <michaelaccetto@gmail.com>
2019-06-17 22:10:28 +0200

    Correct model with bugs

---
+++ utils.py
@@ -0,0 +1,15 @@
+'''
+# notes from mask RCNN paper
+
+## Components
+
+1. Faster-RCNN
+2. Mask branch
+3. RoIAlign
+	* quantization-free layer
+	* Decoupled mask and class predictions
+		- binary masks without class competition
+		- RoI classification branch predicts category. 
+'''
+
+
